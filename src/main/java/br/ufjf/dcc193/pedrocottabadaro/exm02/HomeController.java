7f5ee880c41d677d03816c253521e295c741db66
Pedro Cotta Badaro <pedrocottabadaro@ice.ufjf.br>
2021-07-04 12:36:57 -0300

    add camada de visao com jsp

--- src/main/java/br/ufjf/dcc193/pedrocottabadaro/exm02/HomeController.java
+++ src/main/java/br/ufjf/dcc193/pedrocottabadaro/exm02/HomeController.java
@@ -2,14 +2,12 @@ package br.ufjf.dcc193.pedrocottabadaro.exm02;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     
     @RequestMapping({"","index.html"})
-    @ResponseBody
     public String home(){
-        return "Hello World";
+        return "home";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@page pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>home</h1>
+    <p>hello</p>
+</body>
+</html>
